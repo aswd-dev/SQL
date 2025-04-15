740fc442cb5294374a836d18ebab3431dba9e69e
aswd-dev <muhamedjakupi745@gmail.com>
2025-04-15 10:43:47 +0200

    Add files via upload

---
+++ SQLQuery-Homework Class04.sql
@@ -0,0 +1,43 @@
+-- Class 04
+
+DECLARE @FirstName VARCHAR(100)
+SET @FirstName = 'Antonio'
+
+SELECT *
+FROM Student
+WHERE FirstName = @FirstName
+
+DECLARE @FemaleStudent TABLE
+(StudentId INT NOT NULL, StudentName NVARCHAR(100), DateOfBirth datetime)
+
+INSERT INTO @FemaleStudent (StudentId, StudentName, DateOfBirth)
+SELECT Id, FirstName + ' ' + LastName AS StudentName, DateOfBirth
+FROM Student
+WHERE Gender = 'F';
+
+SELECT * FROM @FemaleStudent
+
+-- Temp Table
+
+DROP TABLE IF EXISTS #MaleStudentTempTable
+
+CREATE TABLE #MaleStudentTempTable
+(LastName NVARCHAR(100), EnrolledDate Datetime)
+
+INSERT INTO #MaleStudentTempTable
+SELECT LastName, EnrolledDate
+FROM Student
+WHERE Gender = 'M' AND FirstName LIKE '%A'
+
+SELECT *
+FROM #MaleStudentTempTable
+WHERE LEN(LastName) = 7
+
+
+
+-- Find all teachers whose FirstName length is less than 5 and
+-- the first 3 characters of their FirstName and LastName are the same
+
+SELECT *
+FROM Teacher
+WHERE LEN(FirstName) < 5 AND LEFT (FirstName, 3) = LEFT (LastName, 3)
